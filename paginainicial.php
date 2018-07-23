4c8fe5d26b71646a26186aad1e37a0597c396efd
DERICK MASUKO OLIVEIRA <derick.moliveira1@senacsp.edu.br>
2018-07-23 16:42:02 -0300

    ...

--- paginainicial.php
+++ paginainicial.php
@@ -1,36 +1,49 @@
+<link rel="stylesheet" type="text/css" href="css/paginainicial.css"/>
+
 <?php 
 	include("inc/topo.php");
 	include("processos/dao.pergunta.php");
 	
 	$perguntaDAO = new PerguntaDAO();
 	$perguntas = $perguntaDAO->ListarPergunta();
-	$perguntaPosicao = rand(0,count($perguntas)-1);
+	// $perguntaPosicao = rand(0,count($perguntas)-1);
 
-	$pergunta = $perguntas[$perguntaPosicao];
-	
+	$pergunta = $perguntas[0];
+	$pergunta2 = $perguntas[1];
+	$pergunta3 = $perguntas[2];
+	$pergunta4 = $perguntas[3];
 
 ?>
 
 
 <?php
 $today = date_default_timezone_set('America/Sao_Paulo');
-$time = 1449;
-$time2 = 1457;
+$time = 0700;
+$time2 = 1300;
+$time3 = 1900;
+$time4 = 2300;
 $current_time = (int) date('Hi');
 if($current_time >= $time && $current_time < $time2) {
-    echo "O que você entende por Resiliência?";
+    echo "<h1>$pergunta[perguntas]</h1>";
 }
 
 	else if ($current_time >= $time2) {
-	echo "xxxxxxxxxxxxxxxxxxxx";
+	echo "<h1>$pergunta2[perguntas]</h1>";
+	}
+
+	else if ($current_time >= $time3) {
+		echo "<h1>$pergunta3[perguntas]</h1>";
+	}
+
+	else if ($current_time >= $time4 && $current_time < $time) {
+		echo "<h1>$pergunta4[perguntas]</h1>";
 	}
 ?>
 
 
-<link rel="stylesheet" type="text/css" href="css/paginainicial.css"/>
+
 <div id="posts">
 
-			<h1><?=$pergunta["perguntas"];?></h1>
 	
 			<form action="post_grava.php" method="post" id="publicar">
 				<input type="hidden" name="idpergunta" value="<?=$pergunta["id"];?>"/>
