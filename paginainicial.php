714d9a2a5151f8de9572a27a0d17817fa4e8d2ce
Bruno <b.silvaclementin@gmail.com>
2018-07-24 21:23:03 -0300

    atualizado

--- paginainicial.php
+++ paginainicial.php
@@ -3,9 +3,33 @@
 <?php 
 	include("inc/topo.php");
 	include("processos/dao.pergunta.php");
+
+	$today = date_default_timezone_set('America/Sao_Paulo');
+	$time = 070000;
+	$time2 = 130000;
+	$time3 = 190000;
+	$time4 = 230000;
+	$current_time = (int) date('Hi');
 	
 	$perguntaDAO = new PerguntaDAO();
-	$perguntas = $perguntaDAO->ListarPergunta();
+	// $perguntas = $perguntaDAO->ListarPergunta();
+	if($current_time >= $time && $current_time < $time2){
+		$perguntas = $perguntaDAO->ListarDataHora1($time);
+		$pergunta = $perguntas[0];
+	}
+	else if ($current_time >= $time2 && $current_time < $time3){
+		$perguntas = $perguntaDAO->ListarDataHora2($time2);
+		$pergunta = $perguntas[0];
+	}
+	else if ($current_time >= $time3 && $current_time < $time4){
+		$perguntas = $perguntaDAO->ListarDataHora3($time3);
+		$pergunta = $perguntas[0];
+	}
+	else if ($current_time >= $time4 || $current_time <= $time){
+		$perguntas = $perguntaDAO->ListarDataHora4($time4);
+		$pergunta = $perguntas[0];
+	}
+
 /*	$perguntaPosicao = count($perguntas);
 
 	$perguntaPosicao = count($perguntas);
@@ -51,18 +75,13 @@ if($current_time >= $time && $current_time < $time2) {
 
 
 ?>
-
-
-
-<div id="posts">
-
-	
-			<form action="post_grava.php" method="post" id="publicar">
-				<input type="hidden" name="idpergunta" value="<?=$perguntas["id"];?>"/>
-				<textarea name="textoresposta" placeholder="O que você pensa sobre isso?" id="textoresposta"></textarea>
-				<input type="submit" id="submit" value="Publicar"/>
-			</form>
-			
+	<h1><?=$pergunta["perguntas"]?></h1>
+	<div id="posts">	
+		<form action="post_grava.php" method="post" id="publicar">
+			<input type="hidden" name="idpergunta" value="<?=$perguntas["id"];?>"/>
+			<textarea name="textoresposta" placeholder="O que você pensa sobre isso?" id="textoresposta"></textarea>
+			<input type="submit" id="submit" value="Publicar"/>
+		</form>			
 	</div>
 
 
--- processos/dao.pergunta.php
+++ processos/dao.pergunta.php
@@ -13,7 +13,120 @@
 			
 			return $lista;
 		}
+		
+		 function ListarDataHora1(){
+			$resultado = $this->query("SELECT perguntas, CURDATE(), TIME('07:00:00') FROM pergunta  LIMIT 1 ");
+			$resultado->setFetchMode(PDO::FETCH_ASSOC);
+			$pergunta = $resultado->fetchAll();
+			
+			return $pergunta;
+		}
+		
+		function ListarDataHora2(){
+			$resultado = $this->query("SELECT perguntas, CURDATE(), TIME('13:00:00') FROM pergunta  LIMIT 1 ");
+			$resultado->setFetchMode(PDO::FETCH_ASSOC);
+			$pergunta = $resultado->fetchAll();
+			
+			return $pergunta;
+		}
+		
+		function ListarDataHora3(){
+			$resultado = $this->query("SELECT perguntas, CURDATE(), TIME('19:00:00')  FROM pergunta  LIMIT 1 ");
+			$resultado->setFetchMode(PDO::FETCH_ASSOC);
+			$pergunta = $resultado->fetchAll();
+			
+			return $pergunta;
+		}
+		
+		function ListarDataHora4(){
+			$resultado = $this->query("SELECT perguntas, CURDATE(), TIME('23:00:00') FROM pergunta  LIMIT 1 ");
+			$resultado->setFetchMode(PDO::FETCH_ASSOC);
+			$pergunta = $resultado->fetchAll();
+			
+			return $pergunta;
+		} 
+		
+	/*	function ListarDataHora1($codigo){
+			$comando = $this->prepare("SELECT perguntas, CURDATE() FROM pergunta WHERE TIME = (?) ");
+			
+			try{
+				$comando->execute(array($codigo));
+				$comando->setFetchMode(PDO::FETCH_ASSOC);
+				$resultado = $comando->fetchAll();
+				
+			}
+			catch(Exception $e){
+				$this->Mensagem = $e->getMessage();
+			}
+								
+			$cat = null;
+			if (count($resultado)==1){
+				$cat = $resultado[0];			
+		}
+			return $cat;
+		
+	}
+		function ListarDataHora2($codigo){
+			$comando = $this->prepare("SELECT perguntas, CURDATE() FROM pergunta WHERE TIME = (?) ");
+			
+			try{
+				$comando->execute(array($codigo));
+				$comando->setFetchMode(PDO::FETCH_ASSOC);
+				$resultado = $comando->fetchAll();
+				
+			}
+			catch(Exception $e){
+				$this->Mensagem = $e->getMessage();
+			}
+								
+			$cat = null;
+			if (count($resultado)==1){
+				$cat = $resultado[0];			
+		}
+			return $cat;
+		
+	}
+		function ListarDataHora3($codigo){
+			$comando = $this->prepare("SELECT perguntas, CURDATE() FROM pergunta WHERE TIME = (?) ");
+			
+			try{
+				$comando->execute(array($codigo));
+				$comando->setFetchMode(PDO::FETCH_ASSOC);
+				$resultado = $comando->fetchAll();
+				
+			}
+			catch(Exception $e){
+				$this->Mensagem = $e->getMessage();
+			}
+								
+			$cat = null;
+			if (count($resultado)==1){
+				$cat = $resultado[0];			
+		}
+			return $cat;
+		
+	}
+		function ListarDataHora4($codigo){
+			$comando = $this->prepare("SELECT perguntas, CURDATE() FROM pergunta WHERE TIME = (?) ");
+			
+			try{
+				$comando->execute(array($codigo));
+				$comando->setFetchMode(PDO::FETCH_ASSOC);
+				$resultado = $comando->fetchAll();
+				
+			}
+			catch(Exception $e){
+				$this->Mensagem = $e->getMessage();
+			}
+								
+			$cat = null;
+			if (count($resultado)==1){
+				$cat = $resultado[0];			
+		}
+			return $cat;
+		
+	}
 	
-	
+	*/
 	}
 ?>
