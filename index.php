f113a85511c20d24ef5f75209cb8e05870ec45ae
DERICK MASUKO OLIVEIRA <derick.moliveira1@senacsp.edu.br>
2018-09-14 13:43:11 -0300

    .

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <!doctype html>
-<html>
+<html lang="pt-br">
 <head>	
 <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="css/indexlogin.css"/>
 	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
@@ -21,8 +22,7 @@
 	$usuariosDAO = new RegistroDAO();
 	
 	if(isset($_SESSION["usuario"]) || isset($_SESSION["usuario.nome"])) {
-	$buscarEmail = $usuariosDAO->BuscarEmail($_SESSION["usuario.email"]) ;}
-	
+	$buscarEmail = $usuariosDAO->BuscarEmail($_SESSION["usuario.email"]) ;}	
 	
 	
 	$today = date_default_timezone_set('America/Sao_Paulo');
