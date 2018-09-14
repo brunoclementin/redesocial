e0d5a182760a91f96d088dae1cb4ae0997451e3d
BRUNO SILVA <bruno.silva423@fatec.sp.gov.br>
2018-09-14 14:16:42 -0300

    ok

--- processos/dao.post.php
+++ processos/dao.post.php
@@ -26,7 +26,7 @@
 			$resultado = $this->query("SELECT p.*, ask.perguntas as perg, u.foto as usuariofoto, u.nome as usuarionome,
 									  u.fotocapa as usuariocapa
 									  FROM posts p inner join pergunta ask ON (p.id_pergunta = ask.id_per)
-									  	   inner join users u ON (p.id_user = u.id) ORDER by data ASC
+									  	   inner join users u ON (p.id_user = u.id) ORDER by data DESC
 									  ");
 			$resultado->setFetchMode(PDO::FETCH_ASSOC);
 			$lista = $resultado->fetchAll();
