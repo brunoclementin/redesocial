aa80436f2b9e466fb247f405819a71354a29cd55
DERICK MASUKO OLIVEIRA <derick.moliveira1@senacsp.edu.br>
2018-09-14 14:11:35 -0300

    k

--- index.php
+++ index.php
@@ -2,13 +2,9 @@
 <html lang="pt-br">
 <head>	
 <meta charset="utf-8">
-<<<<<<< HEAD
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-=======
 	
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
->>>>>>> origin/master
 	<link rel="stylesheet" href="css/indexlogin.css"/>
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -47,14 +43,10 @@
 	
 	require_once("processos/dao.registro.php");
 	$usuariosDAO = new RegistroDAO();
-<<<<<<< HEAD
 	
 	if(isset($_SESSION["usuario"]) || isset($_SESSION["usuario.nome"])) {
 	$buscarEmail = $usuariosDAO->BuscarEmail($_SESSION["usuario.email"]) ;}	
 	
-=======
-
->>>>>>> origin/master
 	
 	$today = date_default_timezone_set('America/Sao_Paulo');
 	$time = 0700;
